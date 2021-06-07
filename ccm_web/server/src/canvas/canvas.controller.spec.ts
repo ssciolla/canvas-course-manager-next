a17af78227d14ac9c40179ac7bbd29c07cd7cb37
Sam Sciolla <ssciolla@umich.edu>
2021-06-07 16:48:20 -0400

    Add defined tests for CanvasService, CanvasController using mocks

--- ccm_web/server/src/canvas/canvas.controller.spec.ts
+++ ccm_web/server/src/canvas/canvas.controller.spec.ts
@@ -1,18 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CanvasController } from './canvas.controller';
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { CanvasController } from './canvas.controller'
+import { CanvasService } from './canvas.service'
+
+const mockCanvasService = {
+  getAuthURL: jest.fn(),
+  createTokenForUser: jest.fn(),
+  findToken: jest.fn()
+}
 
 describe('CanvasController', () => {
-  let controller: CanvasController;
+  let controller: CanvasController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [CanvasController],
-    }).compile();
+      providers: [
+        {
+          provide: CanvasService,
+          useValue: mockCanvasService
+        }
+      ],
+      controllers: [CanvasController]
+    }).compile()
 
-    controller = module.get<CanvasController>(CanvasController);
-  });
+    controller = module.get<CanvasController>(CanvasController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
--- ccm_web/server/src/canvas/canvas.service.spec.ts
+++ ccm_web/server/src/canvas/canvas.service.spec.ts
@@ -1,14 +1,52 @@
-import { ConfigModule, ConfigService } from '@nestjs/config'
+import { HttpModule } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+
+import { CanvasToken } from './canvas.model'
 import { CanvasService } from './canvas.service'
+
+import { UserService } from '../user/user.service'
+import { getModelToken } from '@nestjs/sequelize'
+
+const mockUserService = {
+  upsertUser: jest.fn(),
+  findUserByLoginId: jest.fn()
+}
+
+const mockCanvasTokenModel = {
+  create: jest.fn()
+}
+
+const mockCanvasConfig = {
+  instanceURL: 'https://example.instructure.edu',
+  apiClientId: 'some long number',
+  apiSecret: 'some long code'
+}
+
+const mockGetCanvasConfig = jest.fn().mockReturnValue(mockCanvasConfig)
+const mockConfigService = { get: mockGetCanvasConfig }
 
 describe('CanvasService', () => {
   let service: CanvasService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule],
-      providers: [CanvasService, ConfigService]
+      imports: [HttpModule],
+      providers: [
+        CanvasService,
+        {
+          provide: UserService,
+          useValue: mockUserService
+        },
+        {
+          provide: getModelToken(CanvasToken),
+          useValue: mockCanvasTokenModel
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService
+        }
+      ]
     }).compile()
 
     service = module.get<CanvasService>(CanvasService)
